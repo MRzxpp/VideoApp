fd063a13c29ef2784cfb6758abeb067177d7cca8
sz.zhong <542481524@qq.com>
2016-10-18 20:33:18 +0800

    pages adapter

--- app/src/main/java/com/haishanda/android/videoapp/Activity/MainActivity.java
+++ app/src/main/java/com/haishanda/android/videoapp/Activity/MainActivity.java
@@ -1,38 +1,31 @@
 package com.haishanda.android.videoapp.Activity;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.MotionEvent;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ImageButton;
-import android.widget.Spinner;
 
+import com.haishanda.android.videoapp.Fragement.BoatFragment;
+import com.haishanda.android.videoapp.Fragement.MonitorFragment;
+import com.haishanda.android.videoapp.Fragement.MyFragment;
+import com.haishanda.android.videoapp.Fragement.PhotosFragment;
 import com.haishanda.android.videoapp.R;
 import com.hyphenate.chat.EMClient;
 import com.hyphenate.chat.EMOptions;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
-public class MainActivity extends Activity {
-    private List<String> list = new ArrayList<String>();
-    //    private TextView myTextView;
-    private ArrayAdapter<String> adapter;
-    private ImageButton myButton;
-    @BindView(R.id.spinner_index_choices)
-    Spinner mySpinner;
+public class MainActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+        setDefaultFragment();
         //初始化环信
         EMOptions options = new EMOptions();
         options.setAcceptInvitationAlways(false);
@@ -44,54 +37,6 @@ public class MainActivity extends Activity {
         EMClient.getInstance().init(this, options);
 //在做打包混淆时，关闭debug模式，避免消耗不必要的资源
         EMClient.getInstance().setDebugMode(true);
-
-        list.add("我的船舶");
-        list.add("我的相册2");
-        list.add("监控");
-        list.add("我的账户");
-//        myTextView = (TextView)findViewById(R.id.TextView_city);
-//        mySpinner = (Spinner) findViewById(R.id.spinner_index_choices);
-        //第二步：为下拉列表定义一个适配器，这里就用到里前面定义的list。
-        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, list);
-        //第三步：为适配器设置下拉列表下拉时的菜单样式。
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        //第四步：将适配器添加到下拉列表上
-        mySpinner.setAdapter(adapter);
-        //第五步：为下拉列表设置各种事件的响应，这个事响应菜单被选中
-        mySpinner.setOnItemSelectedListener(new Spinner.OnItemSelectedListener() {
-            public void onItemSelected(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
-                // TODO Auto-generated method stub
-                /* 将所选mySpinner 的值带入myTextView 中*/
-//                myTextView.setText("您选择的是："+ adapter.getItem(arg2));
-                /* 将mySpinner 显示*/
-                arg0.setVisibility(View.VISIBLE);
-            }
-
-            public void onNothingSelected(AdapterView<?> arg0) {
-                // TODO Auto-generated method stub
-//                myTextView.setText("NONE");
-                arg0.setVisibility(View.VISIBLE);
-            }
-        });
-        /*下拉菜单弹出的内容选项触屏事件处理*/
-        mySpinner.setOnTouchListener(new Spinner.OnTouchListener() {
-            public boolean onTouch(View v, MotionEvent event) {
-                // TODO Auto-generated method stub
-                /**
-                 *
-                 */
-                return false;
-            }
-        });
-        /*下拉菜单弹出的内容选项焦点改变事件处理*/
-        mySpinner.setOnFocusChangeListener(new Spinner.OnFocusChangeListener() {
-            public void onFocusChange(View v, boolean hasFocus) {
-                // TODO Auto-generated method stub
-
-            }
-        });
-
-
     }
 
     public void skipToLoginPage(View view) {
@@ -99,4 +44,45 @@ public class MainActivity extends Activity {
         intent.setClass(MainActivity.this, LoginActivity.class);
         startActivity(intent);
     }
+
+    private void setDefaultFragment() {
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction transaction = fm.beginTransaction();
+        BoatFragment boatFragment = new BoatFragment();
+        transaction.replace(R.id.fragment_main, boatFragment);
+        transaction.commit();
+    }
+
+    @OnClick({R.id.to_boat_fragment, R.id.to_photos_fragment, R.id.to_monitor_fragment, R.id.to_my_fragment})
+    public void skipToBoatFragment(View view) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        switch (view.getId()) {
+            case (R.id.to_boat_fragment): {
+                BoatFragment boatFragment = new BoatFragment();
+                fragmentTransaction.replace(R.id.fragment_main, boatFragment);
+                break;
+            }
+            case (R.id.to_photos_fragment): {
+                PhotosFragment photosFragment = new PhotosFragment();
+                fragmentTransaction.replace(R.id.fragment_main, photosFragment);
+                break;
+            }
+            case (R.id.to_monitor_fragment): {
+                MonitorFragment monitorFragment = new MonitorFragment();
+                fragmentTransaction.replace(R.id.fragment_main, monitorFragment);
+                break;
+            }
+            case (R.id.to_my_fragment): {
+                MyFragment myFragment = new MyFragment();
+                fragmentTransaction.replace(R.id.fragment_main, myFragment);
+                break;
+            }
+            default:
+                break;
+        }
+        fragmentTransaction.commit();
+
+    }
+
 }
--- app/src/main/java/com/haishanda/android/videoapp/Fragement/BoatFragment.java
+++ app/src/main/java/com/haishanda/android/videoapp/Fragement/BoatFragment.java
@@ -3,19 +3,88 @@ package com.haishanda.android.videoapp.Fragement;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+import android.widget.Spinner;
 
+import com.haishanda.android.videoapp.Activity.MainActivity;
 import com.haishanda.android.videoapp.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by Zhongsz on 2016/10/14.
  */
 
 public class BoatFragment extends Fragment {
+    private List<String> list = new ArrayList<String>();
+    //    private TextView myTextView;
+    private ArrayAdapter<String> adapter;
+    @BindView(R.id.spinner_index_choices)
+    Spinner mySpinner;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_boat, container, false);
+        View view = inflater.inflate(R.layout.fragment_boat, container, false);
+        ButterKnife.bind(this, view);
+        dealSpinner();
+        return view;
+    }
+
+    private void dealSpinner() {
+        list.add("我的船舶");
+        list.add("我的相册2");
+        list.add("监控");
+        list.add("我的账户");
+//        myTextView = (TextView)findViewById(R.id.TextView_city);
+//        mySpinner = (Spinner) findViewById(R.id.spinner_index_choices);
+        //第二步：为下拉列表定义一个适配器，这里就用到里前面定义的list。
+        adapter = new ArrayAdapter<String>(getActivity().getBaseContext(), android.R.layout.simple_spinner_dropdown_item, list);
+        //第三步：为适配器设置下拉列表下拉时的菜单样式。
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        //第四步：将适配器添加到下拉列表上
+        mySpinner.setAdapter(adapter);
+        //第五步：为下拉列表设置各种事件的响应，这个事响应菜单被选中
+        mySpinner.setOnItemSelectedListener(new Spinner.OnItemSelectedListener() {
+            public void onItemSelected(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+                // TODO Auto-generated method stub
+                /* 将所选mySpinner 的值带入myTextView 中*/
+//                myTextView.setText("您选择的是："+ adapter.getItem(arg2));
+                /* 将mySpinner 显示*/
+                arg0.setVisibility(View.VISIBLE);
+            }
+
+            public void onNothingSelected(AdapterView<?> arg0) {
+                // TODO Auto-generated method stub
+//                myTextView.setText("NONE");
+                arg0.setVisibility(View.VISIBLE);
+            }
+        });
+        /*下拉菜单弹出的内容选项触屏事件处理*/
+        mySpinner.setOnTouchListener(new Spinner.OnTouchListener() {
+            public boolean onTouch(View v, MotionEvent event) {
+                // TODO Auto-generated method stub
+                /**
+                 *
+                 */
+                return false;
+            }
+        });
+        /*下拉菜单弹出的内容选项焦点改变事件处理*/
+        mySpinner.setOnFocusChangeListener(new Spinner.OnFocusChangeListener() {
+            public void onFocusChange(View v, boolean hasFocus) {
+                // TODO Auto-generated method stub
+
+            }
+        });
     }
 }
